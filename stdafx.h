3b9cfc4ece088ed316e994ece4a5cfd76b15ab28
Landratt, LLC <Landratt.LLC@gmail.com>
2019-04-10 17:48:27 -0700

    Create stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,31 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+
+
+
+using namespace std;
+
+const int bonusSize = 11;
+const int requirementSize = 3;
+const int typesSize = 7;
+
+extern char choiceYN();
+
+
+// TODO: reference additional headers your program requires here
